5bc683790dc90d20d83f590deda6f454411c95c2
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-06-23 16:52:24 +0200

    Use type predicates

---
+++ dir/exer4.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.persons = void 0;
+exports.isAdmin = isAdmin;
+exports.isUser = isUser;
+exports.logPerson = logPerson;
+exports.persons = [
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
+];
+function isAdmin(person) {
+    return person.type === 'admin';
+}
+function isUser(person) {
+    return person.type === 'user';
+}
+function logPerson(person) {
+    let additionalInformation = '';
+    if (isAdmin(person)) {
+        additionalInformation = person.role;
+    }
+    if (isUser(person)) {
+        additionalInformation = person.occupation;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+console.log('Admins:');
+exports.persons.filter(isAdmin).forEach(logPerson);
+console.log();
+console.log('Users:');
+exports.persons.filter(isUser).forEach(logPerson);
