1d638d81f5e8ef440ac42c12ff286ffa59069f89
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-06-25 13:56:49 +0200

    The gate about generic function identity using arrow function

--- src/generic1.ts
+++ src/generic1.ts
@@ -1,7 +1,10 @@
 // Write a generic function identity that returns the value it receives without changing the type.
 
-function identity<T> (value: T): T {
-    return value;
-}
+// function identity<T> (value: T): T {
+//     return value;
+// }
 
-console.log(identity([12,12]));
+// console.log(identity([12,12]));
+
+//Using arrow functions
+const identity = <T>(value: T): T => value;
---
+++ src/typeGuard1.ts
@@ -0,0 +1,4 @@
+// Write a TypeScript function processValue that takes string | number | boolean | null | undefined, uses type 
+// guards to log uppercase strings, numbers fixed to two decimals, and boolean as "True"/"False,” and throws 
+// an error for null or undefined, including a type assertion to narrow the type.
+
