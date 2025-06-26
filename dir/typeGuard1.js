40ff76eab60d25a64cf7f742fc4f2d43b9865ed2
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-06-26 06:20:05 +0200

    3rd question alu gate, typeGuards

--- dir/typeGuard1.js
+++ dir/typeGuard1.js
@@ -1,1 +1,16 @@
 "use strict";
+const processValue = (value) => {
+    if (typeof value === "string") {
+        console.log(value.toUpperCase());
+    }
+    else if (typeof value === "number") {
+        console.log(value.toFixed(2));
+    }
+    else if (typeof value === "boolean") {
+        console.log(value ? "True" : "False");
+    }
+    else {
+        throw new Error(`Invalid value`);
+    }
+};
+console.log(processValue(8));
--- src/typeGuard1.ts
+++ src/typeGuard1.ts
@@ -1,4 +1,17 @@
-// Write a TypeScript function processValue that takes string | number | boolean | null | undefined, uses type 
-// guards to log uppercase strings, numbers fixed to two decimals, and boolean as "True"/"False,” and throws 
-// an error for null or undefined, including a type assertion to narrow the type.
+// Write a TypeScript function processValue that takes string | number | boolean | null | undefined,
+// uses type guards to log uppercase strings, numbers fixed to two decimals, and boolean as "True"/"False,” 
+// and throws an error for null or undefined, including a type assertion to narrow the type.
 
+const processValue = (value: string | number | boolean | null | undefined): void => {
+  if (typeof value === "string") {
+    console.log(value.toUpperCase());
+  } else if (typeof value === "number") {
+    console.log(value.toFixed(2));
+  } else if (typeof value === "boolean") {
+    console.log(value ? "True" : "False");
+  } else {
+    throw new Error(`Invalid value`);
+  }
+};
+
+console.log(processValue(8))
